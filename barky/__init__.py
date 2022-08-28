8336f36095351390cd5bd6b5f04293b97c686264
alvaro <garciajaenalvaro@gmail.com>
2022-08-29 00:35:34 +0200

    replicate core functionality in python3

---
+++ barky/__init__.py
@@ -0,0 +1,26 @@
+from notifypy import Notify
+import telegram
+import os
+
+
+def notify_locally(title: str, body: str):
+    notification = Notify()
+    notification.title = title
+    notification.message = body
+
+    notification.send()
+
+
+def notify_remotelly(title: str, body: str):
+    chat_id = os.environ["BARKY_TG_CHAT"]
+    token = os.environ["BARKY_TG_TOKEN"]
+
+    text = "{}\n\n{}".format(title, body)
+
+    bot = telegram.Bot(token)
+    bot.send_message(chat_id, text)
+
+
+def main():
+    notify_locally("Title", "This is the body")
+    notify_remotelly("Title", "This is the body")
