3e86223d2e77e47a1a9a535a2efa49bbf9a08182
alvaro <garciajaenalvaro@gmail.com>
2022-08-29 12:34:08 +0200

    change argument body to option message

--- barky/__main__.py
+++ barky/__main__.py
@@ -3,17 +3,21 @@ import barky
 
 
 @click.command()
-@click.argument("body")
 @click.option("-t", "--title", type=str)
+@click.option("-m", "--message", type=str)
 @click.option("-r", "--remote", is_flag=True)
 @click.option("-l", "--local", is_flag=True)
-def main(body, title, remote, local):
+def main(message, title, remote, local):
+    message
+
     title = title if title else "[+] Barky"
+    message = message if message else "PING"
+
     if remote:
-        barky.notify_remotelly(title, body)
+        barky.notify_remotelly(title, message)
     if local:
-        barky.notify_locally(title, body)
+        barky.notify_locally(title, message)
 
     if not remote and not local:
-        barky.notify_remotelly(title, body)
-        barky.notify_locally(title, body)
+        barky.notify_remotelly(title, message)
+        barky.notify_locally(title, message)
